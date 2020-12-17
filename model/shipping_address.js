0124e23460c13e5ff0605b647c3b69d7b183bdcd
kavita <sfs.kavita19@gmail.com>
2020-12-17 12:17:30 +0530

    first commit

--- model/shipping_address.js
+++ model/shipping_address.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema
 var address = new Schema({
-    token : {type : String , required : true},
+    token : {type : String },
+    customer_id:{type : mongoose.Types.ObjectId},
 address_line1 : {type:String , required : true},
 address_line2 : {type :String , required : true},
 city : {type :String,required : true},
